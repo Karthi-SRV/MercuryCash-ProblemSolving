c362c7e7e44862bae7f5cf023509bb159b68fe2c
Karthi SRV <karthivkm619@gmail.com>
2020-02-17 15:07:25 +0530

    Lint fixed

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
-
 // parse application/json
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json())
 
+app.use(bodyParser.json())
+// Lister to port
 app.listen(8081);
 
 app.post('/sum', function (req, res) {
@@ -22,8 +22,8 @@ app.post('/sum', function (req, res) {
 	      message: "Required Param Not found"
 	    } );
  	}
-    const data = req.body.data;
-    const subArrayLength = req.body.length;
+	const data = req.body.data;
+
   	if(data === null || data.length == 0) {
 	    return res.send({
 	      status: false,
@@ -31,35 +31,44 @@ app.post('/sum', function (req, res) {
 	    } );
   	}
   
-    if(data.some(isNaN)) {
-        return res.send({
-        status: false,
-        message: "Data should contain only numeric value"
-        } );
-    }
-    let definedLength = data.length
-    const responseData = maxSum(data, definedLength, subArrayLength)
-    return res.send({
-        status: true,
-        data: responseData
-        } );
+  	if(data.some(isNaN)) {
+	    return res.send({
+	      status: false,
+	      message: "Data should contain only numeric value"
+	    } );
+  	}
+
+  	const responseData = maxSum(data, data.length)
+  	return res.send({
+	  	status: true,
+	  	data: responseData
+	});
 })
+
 
 function maxSum(data, totalSize) {
 	let responseValue = 0;
 	let responseArray = []
+
+	// Create subarray by using the array size
 	for (let i = 0; i < totalSize; i++) {
+		//Assign subarray to empth
 	    responseArray = [];
 	    for(let j=i; j < totalSize; j++) {
+	    	// Push Subarray
 	        responseArray.push(data[j])
 	        let sum = responseArray.reduce((a, b) => {
+	        	// Sum the given array of data
 	            return a + b;
 	        }, 0);
+
+	        // Evaluate the value which is greater
 	        if(sum > responseValue) {
 	            responseValue = sum;
 	            response = { sum, responseArray }
 	        }
 	    }
 	}
-	return response
+	// return the response
+	return response;
 }
