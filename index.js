563de819be387788532453bbb6f6ba51636727ee
Karthi SRV <karthivkm619@gmail.com>
2020-02-17 16:37:05 +0530

    V2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 // parse application/json
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(bodyParser.json())
@@ -16,59 +16,62 @@ app.listen(8081);
 
 app.post('/sum', function (req, res) {
 
- 	if(typeof req.body === undefined || typeof(req.body.data) === undefined) {
- 		return res.send({
-	      status: false,
-	      message: "Required Param Not found"
-	    } );
- 	}
-	const data = req.body.data;
+    if(typeof req.body === undefined || typeof(req.body.data) === undefined) {
+        return res.send({
+            status: false,
+            message: "Required Param Not found"
+        } );
+    }
+    
+    const data = req.body.data;
 
-  	if(data === null || data.length == 0) {
-	    return res.send({
-	      status: false,
-	      message: "Please provide valid input"
-	    } );
-  	}
-  
-  	if(data.some(isNaN)) {
-	    return res.send({
-	      status: false,
-	      message: "Data should contain only numeric value"
-	    } );
-  	}
+    if(data === null || data.length == 0) {
+        return res.send({
+            status: false,
+            message: "Please provide valid input"
+        } );
+    }
 
-  	const responseData = maxSum(data, data.length)
-  	return res.send({
-	  	status: true,
-	  	data: responseData
-	});
+    if(data.some(isNaN)) {
+        return res.send({
+            status: false,
+            message: "Data should contain only numeric value"
+        } );
+    }
+
+    let sumOfSubArrayResponse = sumOfSubArray(data, data.length)
+    let responseData = sumOfSubArrayResponse.reduce(function(prev, current) {
+        return (prev.sum > current.sum) ? prev : current
+    })
+
+    return res.send({
+        status: true,
+        data: {
+            max: responseData,
+            subArray: sumOfSubArrayResponse
+        }
+    });
 })
 
 
-function maxSum(data, totalSize) {
-	let responseValue = 0;
-	let responseArray = []
+function sumOfSubArray(data, totalSize) {
+    let responseValue = 0;
+    let response = []
 
-	// Create subarray by using the array size
-	for (let i = 0; i < totalSize; i++) {
-		//Assign subarray to empth
-	    responseArray = [];
-	    for(let j=i; j < totalSize; j++) {
-	    	// Push Subarray
-	        responseArray.push(data[j])
-	        let sum = responseArray.reduce((a, b) => {
-	        	// Sum the given array of data
-	            return a + b;
-	        }, 0);
+    for (let i = 0; i < totalSize; i++) {
+        for(let j=i; j < totalSize; j++) {
+            subArray = data.slice(i, j+1) // Create sub array from base array
+            let sum = subArray.reduce((a, b) => {
+                return a + b; // Sum the given array of data
+            }, 0);
+            // Push array of object with Sum and its array set
+            response.push({
+                sum,
+                subArray
+            })
+        }
+    }
 
-	        // Evaluate the value which is greater
-	        if(sum > responseValue) {
-	            responseValue = sum;
-	            response = { responseValue, responseArray }
-	        }
-	    }
-	}
-	// return the response
-	return response;
+    // return the response
+    return response;
 }
